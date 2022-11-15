7452babec163f876dc2f4ba8e750dd74e6d5b20f
aslam48 <youngfizzy001@gmail.com>
2022-11-15 20:47:02 +0100

    working

--- src/SideBar.js
+++ src/SideBar.js
@@ -3,19 +3,24 @@ import './SideBar.css'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import DonutLargeIcon from '@mui/icons-material/DonutLarge';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+import ChatIcon from '@mui/icons-material/Chat';
 
 const SideBar = () => {
   return (
     <div className='sidebar'>
        <div className='sidebar__header'>
+       <AccountCircleIcon/>
+        <div className='sidebar__headerRight'>
 
+           <DonutLargeIcon/>
+           <ChatIcon/>
+           <MoreVertIcon/>
+        </div>
        </div>
 
 
        <div className='sidebar__search'>
-<AccountCircleIcon/>
-<DonutLargeIcon/>
-<MoreVertIcon/>
+
        </div>
  
        <div className='sidebar__chats'>
